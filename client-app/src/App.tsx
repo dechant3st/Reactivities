2101f5f7f5c3309846e3d4eccfdb996e4f560083
dechant3st <trinidadralph03@gmail.com>
2022-01-31 01:09:59 +0800

    End of Section 4

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Header, List } from 'semantic-ui-react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { Header, List } from "semantic-ui-react";
+import axios from "axios";
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
       console.log(response);
       setActivities(response.data);
     });
@@ -16,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header as='h2' icon='users' content='Reactivities' />
+      <Header as="h2" icon="users" content="Reactivities" />
       <List>
         {activities.map((activity: any) => (
           <List.Item key={activity.id}>{activity.title}</List.Item>
--- src/API/Controllers/ActivitiesController.cs
+++ src/API/Controllers/ActivitiesController.cs
@@ -1,32 +1,44 @@
-using Persistence;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
 using System;
+using Application.Activities;
+using System.Threading;
 
 namespace API.Controllers
 {
     public class ActivitiesController: BaseApiController
     {
-        private readonly DataContext _context;
-
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        public async Task<ActionResult<List<Activity>>> GetActivities(CancellationToken cancellationToken) 
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query(), cancellationToken);
         }
 
         [HttpGet("{id}")] // activities/id
-        public async Task<ActionResult<Activity>> GetActivity(Guid id) 
+        public async Task<ActionResult<Activity>> GetActivity(Guid id, CancellationToken cancellationToken) 
         {
-            return await _context.Activities.FindAsync(id);
+            return await Mediator.Send(new Details.Query{ Id = id}, cancellationToken);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity, CancellationToken cancellationToken)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }, cancellationToken));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivities(Guid id, Activity activity, CancellationToken cancellationToken)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command {Activity = activity}, cancellationToken));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id, CancellationToken cancellationToken) 
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }, cancellationToken));
         }
     }
 }
