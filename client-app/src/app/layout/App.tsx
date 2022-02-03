ddc9eb62ef64541feab0c602dd3e72214118e576
dechant3st <trinidadralph03@gmail.com>
2022-02-03 09:12:14 +0800

    End of Section 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
@@ -10,13 +10,31 @@ import TestErrors from "../../features/errors/TestErrors";
 import { ToastContainer } from "react-toastify";
 import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
+import LoginForm from "../../features/users/LoginForm";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
+import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
-export default function App() {
+function App() {
   const location = useLocation();
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore]);
+
+  if (!commonStore.appLoaded)
+    return <LoadingComponent content="Loading app..." />;
 
   return (
     <>
       <ToastContainer position="bottom-right" hideProgressBar />
+      <ModalContainer />
       <Routes>
         <Route index element={<HomePage />} />
         <Route
@@ -49,10 +67,13 @@ export default function App() {
             element={<ActivityForm key={location.key} />}
           />
           <Route path="errors" element={<TestErrors />} />
-          <Route path="/server-error" element={<ServerError />} />
+          <Route path="/erver-error" element={<ServerError />} />
+          <Route path="login" element={<LoginForm />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </>
   );
 }
+
+export default observer(App);
